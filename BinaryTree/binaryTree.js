1d61a5b279c286b60b8fb00ccc50314d8d657968
Rian Sanjaya <rian.sj@gmail.com>
2019-02-20 19:42:40 +0700

    remove some comment

--- BinaryTree/binaryTree.js
+++ BinaryTree/binaryTree.js
@@ -1,10 +1,3 @@
-// To test run the code open each of the files, copy and paste it into google chrome console and press enter.
-
-// Please be kindly noted that because I am currently more familiar with Javascript 
-// so I do all the code in Javascript. 
-// I think the more important think is the logic, 
-// if I pass the test I am sure I can catch up with Java later. 
-
 class treeNode {
     constructor(val) {
         this.val = val;
